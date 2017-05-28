bbbe77056c684a23fb31ce4994d142f4421b5018
Boopesh Mahendran <boopeshmahendran@gmail.com>
2017-05-28 20:28:22 +0530

    Created Game Component

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
+import Question from './Question';
+import Board from './Board';
 import './Game.css';
 
 class Game extends Component {
+  constructor() {
+    super();
+    this.state = {
+      displayStep: 0,
+      playerOption: '',
+      playerCoin: ''
+    };
+  }
+  incrementDisplay() {
+    this.setState({
+      displayStep: this.state.displayStep + 1
+    })
+  }
+  setAnswer(answer, value) {
+    this.setState({
+      [answer]: value,
+    })
+    this.incrementDisplay()
+  }
+  resetDisplay() {
+    this.setState({
+      displayStep: 0
+    })
+  }
   render() {
+    let gameString;
+    switch(this.state.displayStep) {
+      case 0: gameString = <Question question='Whom do you want to play?' options={['2 players', 'Computer']}
+      setAnswer={this.setAnswer.bind(this, 'playerOption')} />
+        break
+      case 1: gameString = <Question question = 'What do you choose?' options={['X', 'O']}
+      setAnswer={this.setAnswer.bind(this, 'playerCoin')} />
+        break
+      case 2: gameString = <Board playerOption={this.state.playerOption} playerCoin={this.state.playerCoin}
+      goToMenu={() => this.resetDisplay()} />
+        break
+    }
     return (
       <div className="Game">
+        {gameString}
       </div>
     );
   }
